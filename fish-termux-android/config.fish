350199002845337d661229a96d46438cf05fb892
lukien <kien.luuhoang.uet@gmail.com>
2021-12-27 13:30:39 +0700

    Add fish for android

--- fish-termux-android/config.fish
+++ fish-termux-android/config.fish
@@ -4,18 +4,12 @@ set -g fish_term24bit 1
 
 #----------------------------------- Abbreviations -----------------------------------#
 
-# General
-    abbr -a md 'mkdir'
-    abbr -a cls 'clear'
 # Navigate folder
     abbr -a ... 'cd ../..'
-    abbr -a ccf 'cd ~/.config/fish'                 # cd config fish
-    abbr -a cho 'cd /home/kienlh4ivi/working/source-code/AppHomeScreen'
-    abbr -a che 'cd /home/kienlh4ivi/working/source-code/AppHelp'
-    abbr -a cst 'cd /home/kienlh4ivi/working/source-code/AppStandbyClock'
-    abbr -a cwb 'cd /home/kienlh4ivi/working/build'
+    abbr -a ccf 'cd ~/.config/fish'                                 # cd config fish
+    abbr -a ccc 'cd $PREFIX'
 # Fast edit config
-    abbr -a efc 'vim ~/.config/fish/config.fish'  # edit fish config
+    abbr -a efc 'vim ~/.config/fish/config.fish'                    # edit fish config
     abbr -a efg 'vim ~/.config/fish/functions/fish_greeting.fish'
     abbr -a efp 'vim ~/.config/fish/functions/fish_prompt.fish'
 # Git
@@ -23,15 +17,25 @@ set -g fish_term24bit 1
     abbr -a gcm 'git commit'
     abbr -a gca 'git commit --amend'
     abbr -a grs 'git clean -f -d && git reset --hard HEAD^ && git pull'
-    abbr -a gpo 'git push origin HEAD:refs/for/'
+    abbr -a gpo 'git push origin '
     abbr -a gpl 'git pull'
     abbr -a gcl 'git clone'
     abbr -a gst 'git status'
     abbr -a gdf 'git diff'
+    abbr -a gpom 'git push origin master'
 # Apt
-    abbr -a aud 'sudo apt-get update'
-    abbr -a aug 'sudo apt-get upgrade -y'
-    abbr -a ait 'sudo apt-get install -y'
-    abbr -a aac 'sudo apt-get autoclean'
-    abbr -a aar 'sudo apt-get autoremove'
-    abbr -a arm 'sudo apt remove'
+    abbr -a aud 'apt update'
+    abbr -a aug 'apt upgrade -y'
+    abbr -a ait 'apt install -y'
+    abbr -a aac 'apt autoclean'
+    abbr -a aar 'apt autoremove'
+    abbr -a arm 'apt remove'
+# Pkg
+    abbr -a pud 'pkg update'
+    abbr -a pug 'pkg upgrade -y'
+    abbr -a pit 'pkg install -y'
+
+#------------------------------------- Aliases -------------------------------------#
+    alias q='exit'
+    alias md='mkdir'
+    alias cls='clear'
