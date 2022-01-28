0e5780ce756dec1aff0b3b9d2a8aa270ecdba24d
lulkien <kien.luuhoang.uet@gmail.com>
2022-01-28 17:19:17 +0700

    Modify prompt

--- fish-termux-android/functions/fish_prompt.fish
+++ fish-termux-android/functions/fish_prompt.fish
@@ -47,36 +47,35 @@ function fish_prompt --description 'Write out the prompt'
         set -l stt (git status --short 2>/dev/null)
 
         # Write branch name
+        set_color normal
         set_color $color_label;     echo -n '(git:';
         set_color $color_branch;    echo -n $branch;
         set_color $color_label;     echo -n ')';
     
         # Write status
         test -z "$stt" 
-        and begin; set_color $color_clear; echo -n " $git_clear"; end 
-        or begin;  set_color $color_dirty; echo -n " $git_dirty"; end
+        and begin; set_color $color_clear;  echo -n " $git_clear"; end
+        or begin;  set_color $color_dirty;  echo -n " $git_dirty"; end
 
         string match -qr '^[0-9]+$' $ah
         and begin
             if test $ah -gt 0 -a $bh -eq 0
-                set_color $color_ahead
-                echo -n "[$git_ah$ah]"
+                set_color $color_ahead; echo -n "[$git_ah$ah]"
             else if test $bh -gt 0 -a $ah -eq 0
-                set_color $color_behind
-                echo -n "[$git_bh$bh]"
+                set_color $color_behind; echo -n "[$git_bh$bh]"
             else if test $ah -eq 0 -a $bh -eq 0
                 echo -n ""
             else
-                set_color $color_akw
-                echo -n "$git_awk"
+                set_color $color_akw; echo -n "$git_awk"
             end
         end
     end
 
     set_color -o $color_bracket; echo -n "$open_bracket"
-    set_color -o $color_name;    echo -n "$prompt_name  "
+    set_color    normal
+    set_color    $color_name;    echo -n "$prompt_name  "
     set_color    $color_pwd;     echo -n "$path_str" 
-    set_color -o $color_bracket; echo -n "$close_bracket"
+    set_color    $color_bracket; echo -n "$close_bracket"
     echo (branch_name)
     set_color -o $color_prefix;  echo -n "$prefix"
     set_color    normal 
