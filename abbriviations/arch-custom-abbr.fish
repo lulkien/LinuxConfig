2e09320503f9fb68c63129eec525beb5a3e43828
lulkien <kien.luuhoang.uet@gmail.com>
2022-07-29 06:15:09 +0700

    Add script to install AMD driver

--- abbriviations/arch-custom-abbr.fish
+++ abbriviations/arch-custom-abbr.fish
@@ -2,7 +2,7 @@
 abbr -a pud     'sudo pacman -Sy'
 abbr -a pug     'sudo pacman -Su --noconfirm'
 abbr -a puf     'sudo pacman -Syyu'
-abbr -a pit     'sudo pacman -S --noconfirm'
+abbr -a pit     'sudo pacman -S --noconfirm --needed'
 abbr -a pcl     'sudo pacman -Rns (pacman -Qdttq)'
 abbr -a prm     'sudo pacman -Rns'
 abbr -a pss     'pacman -sS'
