44cb71dcaf60e393d8cfe89281b36f66bc36979b
lulkien <kien.luuhoang.uet@gmail.com>
2022-07-29 06:15:29 +0700

    Add script to install AMD driver

---
+++ some-fish-script/arch-install-driver-radeon.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/fish
+
+# install lib for mesa, vulkan, etc.
+sudo pacman -S --noconfirm --needed lib32-vulkan-radeon libva-mesa-driver vulkan-radeon mesa mesa-demos mesa-vdpau lib32-libva-mesa-driver lib32-mesa lib32-mesa-demos lib32-mesa-vdpau
+
+# install driver
+sudo pacman -S --noconfirm --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
