29122be69e0ae6962025735480e8c4a046bcb3ed
lulkien <kien.luuhoang.uet@gmail.com>
2022-11-20 01:26:44 +0700

    modify install script

--- nvim_config.d/install_config.fish
+++ nvim_config.d/install_config.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/fish
 
+set NVIM_CFG_D (dirname (status --current-filename))
+
 if test -e ~/.config/nvim/init.vim
     printf "Remove old init\n"
     rm -rf ~/.config/nvim/init.vim
@@ -10,5 +12,5 @@ if test -e ~/.config/nvim/plugin
     rm -rf ~/.config/nvim/plugin
 end
 
-ln -s ./nvim/init.vim ~/.config/nvim/init.vim
-ln -s ./nvim/plugin ~/.config/nvim/plugin
+ln -sf $NVIM_CFG_D/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf $NVIM_CFG_D/nvim/plugin ~/.config/nvim/plugin
