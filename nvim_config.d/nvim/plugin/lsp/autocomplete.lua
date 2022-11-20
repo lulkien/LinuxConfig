eff3904bf4599f0b82c51af9268c5259311cdd95
lulkien <kien.luuhoang.uet@gmail.com>
2022-11-20 17:35:00 +0700

    Add scripts

--- nvim_config.d/nvim/plugin/lsp/autocomplete.lua
+++ nvim_config.d/nvim/plugin/lsp/autocomplete.lua
@@ -17,6 +17,21 @@ cmp.setup({
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-e>'] = cmp.mapping.abort(),
                 ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ['<Tab>'] = function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    else
+                        fallback()
+                    end
+                end,
+                ['<S-Tab>'] = function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    else
+                        fallback()
+                    end
+                end
+
             }),
         sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
