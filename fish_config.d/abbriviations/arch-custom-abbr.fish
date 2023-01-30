8bd1d16b38dd90683d39ba53bc850d1a6e5a713c
lulkien <kien.luuhoang.uet@gmail.com>
2023-01-30 22:07:55 +0700

    xx

--- fish_config.d/abbriviations/arch-custom-abbr.fish
+++ fish_config.d/abbriviations/arch-custom-abbr.fish
@@ -1,7 +1,7 @@
 # Package manager
 abbr -a pud     'sudo pacman -Sy'
 abbr -a pug     'sudo pacman -Su --noconfirm'
-abbr -a puf     'sudo pacman -S --noconfirm archlinux-keyring; sudo pacman -Syu'
+abbr -a puf     'sudo pacman -S --noconfirm --needed archlinux-keyring; sudo pacman -Syu'
 abbr -a pit     'sudo pacman -S --noconfirm --needed'
 abbr -a pcl     'sudo pacman -Rns (pacman -Qdttq)'
 abbr -a prm     'sudo pacman -Rns'
