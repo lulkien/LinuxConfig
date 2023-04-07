4d081d34f3ef07e1dba964a3317648303b930e47
lukien <kien.luuhoang.uet@gmail.com>
2023-04-07 17:57:45 +0700

    Reconstruct dotfiles

--- arch-dotfiles/install.sh
+++ arch-dotfiles/install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Install package
+#pacman -Sy fish git neovim
+
 rm -r ~/.config/fish
 cp -r fish ~/.config
+
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
--- nvim_config.d/nvim/plugin/autocmd.vim
+++
@@ -1,3 +0,0 @@
-" Disable auto comment in newline
-autocmd Filetype * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
-
