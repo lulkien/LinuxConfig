e4a3dd03fdfca623b13c2b4c18002ae561cc5351
lulkien <kien.luuhoang.arch@gmail.com>
2023-07-28 22:09:23 +0700

    Change

--- arch-dotfiles/some-arch-scripts/installation/install-plasma.fish
+++ arch-dotfiles/some-arch-scripts/installation/install-plasma.fish
@@ -8,11 +8,13 @@ sudo pacman -S --needed archlinux-keyring
 sudo pacman -S --needed xorg
 
 # Install kde plasma
-sudo pacman -S --needed plasma plasma-wayland-session sddm 
+sudo pacman -S --needed sddm plasma plasma-wayland-session
 sudo pacman -S --needed konsole dolphin kwallet kwalletmanager kate spectacle kdeconnect elisa gwenview ark partitionmanager
 
 # Install other appliactions
-sudo pacman -S --needed firefox htop curl wget pipewire pipewire-pulse lib32-pipewire wireplumber flatpak git base-devel openssh neofetch htop kitty lsb-release neovim vim
+sudo pacman -S --needed firefox kitty
+sudo pacman -S --needed htop wget curl flatpak git base-devel openssh neofetch lsb-release neovim vim gamemode
+sudo pacman -S --needed pipewire pipewire-pulse lib32-pipewire wireplumber
 
 # Install fonts
 sudo pacman -S --needed ttf-jetbrains-mono-nerd ttf-liberation noto-fonts-cjk noto-fonts-emoji otf-codenewroman-nerd
@@ -36,4 +38,11 @@ if not command -sq yay
         cd /tmp/yay
         makepkg -si
     end
+end
+
+# Install nvchad
+echo "Do you wanna install NvChad??? [y/N]"
+read answer
+if test "$answer" = "Y" -o "$answer" = "y"
+    git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 end
