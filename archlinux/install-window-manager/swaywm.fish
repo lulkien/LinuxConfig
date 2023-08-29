55ae8622081f0f43738f3133dbd9da9483f1814a
lulkien <kien.luuhoang.arch@gmail.com>
2023-08-29 23:19:03 +0700

    Change script install window manager for swaywm

--- archlinux/install-window-manager/swaywm.fish
+++ archlinux/install-window-manager/swaywm.fish
@@ -1,111 +1,131 @@
 #!/usr/bin/fish
 
-set_color ECEB7B; echo "[Update package and install latest keyring]"; set_color normal
-sudo pacman -Sy
-sudo pacman -S --needed archlinux-keyring
+function prepare_keyring
+    set_color ECEB7B; echo "[Update package and install latest keyring]"; set_color normal
+    sudo pacman -Sy
+    sudo pacman -S --needed archlinux-keyring
+end
 
-# Check if yay is available
-set_color ECEB7B; echo "[Check yay available]"; set_color normal
-if not command -sq yay
-    echo 'yay is not installed. Do you wanna install it? [Y/n]'
-    read answer
-    # If the answer is yes or empty, install yay
-    if test -z "$answer" -o "$answer" = "Y" -o "$answer" = "y"
-        # Install yay using pacman
-        git clone https://aur.archlinux.org/yay.git /tmp/yay
-        cd /tmp/yay
-        makepkg -si
+function install_aur_helper
+    set_color ECEB7B; echo "[Check yay available]"; set_color normal
+    sudo pacman -S --needed go
+    if not command -sq yay
+        echo 'yay is not installed. Do you wanna install it? [Y/n]'
+        read answer
+        # If the answer is yes or empty, install yay
+        if test -z "$answer" -o "$answer" = "Y" -o "$answer" = "y"
+            # Install yay using pacman
+            git clone https://aur.archlinux.org/yay.git /tmp/yay
+            cd /tmp/yay
+            makepkg -si
+        end
     end
 end
 
-# Install seatd
-set_color ECEB7B; echo "[Install seatd]"; set_color normal
-echo "Install seatd, enable and add current use to group seat. Swaywm need that."
-sudo pacman -S --needed seatd
-sudo systemctl enable --now seatd
-sudo usermod -aG seat $USER
+function install_desktop_environment
+    set_color ECEB7B; echo "[Install seatd]"; set_color normal
+    echo "Install seatd, enable and add current use to group seat. Swaywm need that."
+    sudo pacman -S --needed seatd
+    sudo systemctl enable --now seatd
+    sudo usermod -aG seat $USER
 
-# Install sway applications
-set_color ECEB7B; echo "[Install swaywm and some sway applications]"; set_color normal
-sudo pacman -S --needed sway swayidle swaybg
-yay -S swaylock-effects
-
-# Install fonts
-set_color ECEB7B; echo "[Install some good nerdfonts]"; set_color normal
-sudo pacman -S --needed ttf-jetbrains-mono-nerd ttf-liberation noto-fonts-cjk noto-fonts-emoji otf-codenewroman-nerd
-
-set_color ECEB7B; echo "[Install other applications]"; set_color normal
-sudo pacman -S --needed \
-    firefox kitty wofi waybar lxappearance \
-    gnome-keyring seahorse \
-    ranger htop neovim vim net-tools \
-    grim slurp ffmpeg \
-    pipewire pipewire-pulse lib32-pipewire wireplumber \
-    xorg-xrandr xorg-xwayland \
-    xdg-desktop-portal xdg-desktop-portal-wlr
-
-set_color ECEB7B; echo "[Install development tools]"; set_color normal
-sudo pacman -S --needed python python-pip base-devel rustup npm
-
-set_color ECEB7B; echo "[Install imagemagick and its dependencies]"; set_color normal
-sudo pacman -S --needed imagemagick ghostscript libheif libjxl libraw librsvg libwebp libwmf libxml2 libzip ocl-icd openexr openjpeg2 djvulibre pango
-
-set_color ECEB7B; echo "[Install input method]"; set_color normal
-sudo pacman -S --needed fcitx5-im fcitx5-bamboo
-
-set_color ECEB7B; echo "[Install bluetooth service]"; set_color normal
-echo "Do you wanna install bluetooth? [y/N]"
-read answer
-if test "$answer" = "Y" -o "$answer" = "y"
-    sudo pacman -S --needed bluez bluez-utils
-    systemctl enable --now bluetooth
+    set_color ECEB7B; echo "[Install sway window manager]"; set_color normal
+    sudo pacman -S --needed xorg-xwayland sway swayidle swaybg
+    yay -S swaylock-effects
 end
 
-set_color ECEB7B; echo "[Install dhcpcd service]"; set_color normal
-echo "Do you wanna install dhcpcd? [y/N]"
-read answer
-if test "$answer" = "Y" -o "$answer" = "y"
-    sudo pacman -S --needed dhcpcd
-    sudo systemctl enable --now dhcpcd
+function install_misc
+    set_color ECEB7B; echo "[Install fonts]"; set_color normal
+    sudo pacman -S --needed \
+        ttf-jetbrains-mono-nerd \
+        ttf-liberation \
+        noto-fonts-cjk \
+        noto-fonts-emoji \
+        otf-codenewroman-nerd
+
+    set_color ECEB7B; echo "[Install other applications]"; set_color normal
+    sudo pacman -S \
+        fish git vim neovim kitty \
+        wofi waybar \
+        unzip unarchiver \
+        firefox flatpak \
+        lxappearance nemo \
+        gnome-keyring seahorse \
+        htop neofetch lsb-release \
+        wget curl openssh rsync \
+        grim slurp ffmpeg \
+        wl-clipboard xorg-xrandr \
+        pipewire pipewire-pulse lib32-pipewire wireplumber \
+        xdg-desktop-portal xdg-desktop-portal-wlr
+
+    set_color ECEB7B; echo "[Install fcitx5]"; set_color normal
+    sudo pacman -S --needed fcitx5-im fcitx5-bamboo
+
+    set_color ECEB7B; echo "[Install development tools]"; set_color normal
+    sudo pacman -S --needed python python-pip base-devel rustup npm
+
+    yay -S brave-bin adwaita-dark wl-color-picker swaync
+
+    # set_color ECEB7B; echo "[Install imagemagick and its dependencies]"; set_color normal
+    # sudo pacman -S --needed imagemagick ghostscript libheif libjxl libraw librsvg libwebp libwmf libxml2 libzip ocl-icd openexr openjpeg2 djvulibre pango
 end
 
-set_color ECEB7B; echo "[Install iwd service]"; set_color normal
-echo "Do you wanna install iwd? [y/N]"
-read answer
-if test "$answer" = "Y" -o "$answer" = "y"
-    sudo pacman -S --needed iwd
-    sudo systemctl enable --now iwd
+function enable_services
+    set_color ECEB7B; echo "[Install bluetooth service]"; set_color normal
+    echo "Do you wanna install bluetooth? [y/N]"
+    read answer
+    if test "$answer" = "Y" -o "$answer" = "y"
+        sudo pacman -S --needed bluez bluez-utils blueman
+        systemctl enable --now bluetooth
+    end
+
+    set_color ECEB7B; echo "[Install dhcpcd service]"; set_color normal
+    echo "Do you wanna install dhcpcd? [y/N]"
+    read answer
+    if test "$answer" = "Y" -o "$answer" = "y"
+        sudo pacman -S --needed dhcpcd
+        sudo systemctl enable --now dhcpcd
+    end
+
+    set_color ECEB7B; echo "[Install iwd service]"; set_color normal
+    echo "Do you wanna install iwd? [y/N]"
+    read answer
+    if test "$answer" = "Y" -o "$answer" = "y"
+        sudo pacman -S --needed iwd
+        sudo systemctl enable --now iwd
+    end
 end
 
-set_color ECEB7B; echo "[Copy configuration]"; set_color normal
-echo "Do you want to clone configuration? [Y/n] "
-read answer
-if test -z "$answer" -o "$answer" = "Y" -o "$answer" = "y"
-    echo "Using SSH? [Y/n] "
+function clone_configuations
+    set_color ECEB7B; echo "[Copy configuration]"; set_color normal
+    echo "Do you want to clone configuration? [Y/n] "
     read answer
     if test -z "$answer" -o "$answer" = "Y" -o "$answer" = "y"
-        rm -rf ~/.config/fish ~/.config/sway ~/.config/swaylock ~/.config/waybar ~/.config/kitty
         echo ">>> Clone QSingularisRicer/fish.git"
-        git clone "git@github.com:QSingularisRicer/fish.git" ~/.config/fish
-        echo ">>> Clone QSingularisRicer/sway.git"
-        git clone "git@github.com:QSingularisRicer/sway.git" ~/.config/sway
-        echo ">>> Clone QSingularisRicer/swaylock.git"
-        git clone "git@github.com:QSingularisRicer/swaylock.git" ~/.config/swaylock
-        echo ">>> Clone QSingularisRicer/waybar"
-        git clone "git@github.com:QSingularisRicer/waybar.git" ~/.config/waybar
-        echo ">>> Clone QSingularisRicer/kitty.git"
-        git clone "git@github.com:QSingularisRicer/kitty.git" ~/.config/kitty
-    else
-        rm -rf ~/.config/fish ~/.config/sway ~/.config/swaylock ~/.config/waybar ~/.config/kitty
-        echo ">>> Clone QSingularisRicer/fish.git"
+        rm -rf ~/.config/fish
         git clone "https://github.com/QSingularisRicer/fish.git" ~/.config/fish
+
         echo ">>> Clone QSingularisRicer/sway.git"
+        rm -rf ~/.config/sway
         git clone "https://github.com/QSingularisRicer/sway" ~/.config/sway
+
         echo ">>> Clone QSingularisRicer/swaylock.git"
+        rm -rf ~/.config/swaylock
         git clone "https://github.com/QSingularisRicer/swaylock" ~/.config/swaylock
+
         echo ">>> Clone QSingularisRicer/waybar"
+        rm -rf ~/.config/waybar
         git clone "https://github.com/QSingularisRicer/waybar" ~/.config/waybar
+
+        echo ">>> Clone QSingularisRicer/wofi"
+        rm -rf ~/.config/wofi
+        git clone "https://github.com/QSingularisRicer/wofi.git" ~/.config/wofi
+
         echo ">>> Clone QSingularisRicer/kitty.git"
+        rm -rf ~/.config/kitty
         git clone "https://github.com/QSingularisRicer/kitty" ~/.config/kitty
     end
 end
+
+# MAIN SCRIPT
+prepare_keyring && install_aur_helper && install_desktop_environment && install_misc && enable_services && clone_configuations
