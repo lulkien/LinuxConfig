72ec33a2937f8c5fe8bf40df38de50b5d6432925
lulkien <kien.luuhoang.arch@gmail.com>
2023-11-26 23:54:16 +0700

    Add shtis

--- archlinux/install-graphic-driver/amd/install.fish
+++ archlinux/install-graphic-driver/amd/install.fish
@@ -1,16 +1,14 @@
 #!/usr/bin/env fish
 
 function install_mesa
-    sudo pacman -S --needed \
-        mesa lib32-mesa \
+    sudo pacman -S mesa lib32-mesa \
         mesa-demos lib32-mesa-demos \
         mesa-vdpau lib32-mesa-vdpau \
         libva-mesa-driver lib32-libva-mesa-driver
 end
 
 function install_vulkan_driver
-    sudo pacman -S --needed \
-        vulkan-radeon lib32-vulkan-radeon \
+    sudo pacman -S vulkan-radeon lib32-vulkan-radeon \
         vulkan-mesa-layers lib32-vulkan-mesa-layers \
         vulkan-icd-loader lib32-vulkan-icd-loader
 end
