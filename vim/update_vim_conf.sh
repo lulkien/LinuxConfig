ece9e8d84a08bcef3b481fb5019a4f047a58941d
lulkien <kien.luuhoang.arch@gmail.com>
2023-12-23 22:24:37 +0700

    Update vim configs

--- vim/update_vim_conf.sh
+++ vim/update_vim_conf.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-cp -r .vim/ ~
-cp .vimrc ~
+echo "mkdir -p ~/.vim/autoload"
+mkdir -p ~/.vim/autoload/
+echo '--------------------------------'
+
+echo "wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -O ~/.vim/autoload/plug.vim"
+wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -O ~/.vim/autoload/plug.vim
+echo '--------------------------------'
+
+echo "Copy .vimrc and .vim/ to ~"
+cp -r .vim/ ~/
+cp .vimrc ~/
+echo '--------------------------------'
+
 echo "Done"
