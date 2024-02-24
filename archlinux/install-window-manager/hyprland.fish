29d37404edd6c66035884f0fefa0e3d8af427e0b
lulkien <kien.luuhoang.arch@gmail.com>
2024-02-24 21:33:10 +0700

    Indent fish script

--- archlinux/install-window-manager/hyprland.fish
+++ archlinux/install-window-manager/hyprland.fish
@@ -1,20 +1,24 @@
 #!/usr/bin/env fish
 
 function prepare_keyring
-    set_color ECEB7B; echo "[Update package and install latest keyring]"; set_color normal
+    set_color ECEB7B
+    echo "[Update package and install latest keyring]"
+    set_color normal
     sudo pacman -Sy
     sudo pacman -S --needed archlinux-keyring
 end
 
 function install_aur_helper
-    set_color ECEB7B; echo "[Check paru available]"; set_color normal
+    set_color ECEB7B
+    echo "[Check paru available]"
+    set_color normal
     sudo pacman -S --needed base-devel rustup
     rustup default nightly
     if not command -sq paru
         echo 'paru is not installed. Do you wanna install it? [Y/n]'
         read answer
         # If the answer is yes or empty, install paru
-        if test -z "$answer" -o "$answer" = "Y" -o "$answer" = "y"
+        if test -z "$answer" -o "$answer" = Y -o "$answer" = y
             git clone https://aur.archlinux.org/paru.git /tmp/paru
             cd /tmp/paru
             makepkg -si
@@ -23,19 +27,25 @@ function install_aur_helper
 end
 
 function install_desktop_environment
-    set_color ECEB7B; echo "[Install seatd]"; set_color normal
+    set_color ECEB7B
+    echo "[Install seatd]"
+    set_color normal
     echo "Install seatd, enable and add current use to group seat. Swaywm need that."
-    sudo pacman -S --needed seatd
+    paru -S --needed seatd
     sudo systemctl enable --now seatd
     sudo usermod -aG seat $USER
 
-    set_color ECEB7B; echo "[Install hyprland window manager]"; set_color normal
-    sudo pacman -S --needed \
+    set_color ECEB7B
+    echo "[Install hyprland window manager]"
+    set_color normal
+    paru -S --needed \
         xorg-xwayland hyprland hyprpaper \
         xdg-desktop-portal xdg-desktop-portal-hyprland
 
-    set_color ECEB7B; echo "[Install common components of any window manager]"; set_color normal
-    sudo pacman -S --needed \
+    set_color ECEB7B
+    echo "[Install common components of any window manager]"
+    set_color normal
+    paru -S --needed \
         git fish vim neovim \
         alacritty kitty wofi dunst \
         wget curl openssh rsync \
@@ -43,30 +53,34 @@ function install_desktop_environment
         wl-clipboard unzip unarchiver \
         nemo loupe gnome-keyring seahorse polkit-gnome \
         pipewire pipewire-pulse lib32-pipewire wireplumber \
-        xorg-xrandr ripgrep
-    paru -S eww-wayland swaylock-effects hyprdim grimblast nwg-look-bin hyprpicker
+        xorg-xrandr \
+        eww-wayland swaylock-effects hyprdim grimblast nwg-look-bin hyprpicker
 
-    set_color ECEB7B; echo "[Install input method]"; set_color normal
-    sudo pacman -S --needed fcitx5-im fcitx5-bamboo
-    echo "QT_IM_MODULE=fcitx"   | sudo tee -a /etc/environment
+    set_color ECEB7B
+    echo "[Install input method]"
+    set_color normal
+    paru -S --needed fcitx5-im fcitx5-bamboo
+    echo "QT_IM_MODULE=fcitx" | sudo tee -a /etc/environment
     echo "XMODIFIERS=@im=fcitx" | sudo tee -a /etc/environment
-    echo "SDL_IM_MODULE=fcitx"  | sudo tee -a /etc/environment
-    echo "GLFW_IM_MODULE=ibus"  | sudo tee -a /etc/environment
+    echo "SDL_IM_MODULE=fcitx" | sudo tee -a /etc/environment
+    echo "GLFW_IM_MODULE=ibus" | sudo tee -a /etc/environment
 
-    set_color ECEB7B; echo "[Install other applications]"; set_color normal
-    sudo pacman -S \
-        breeze breeze-gtk \
-        ffmpeg flatpak
-    paru -S floorp-bin
+    set_color ECEB7B
+    echo "[Install other applications]"
+    set_color normal
+    paru -S --needed breeze breeze-gtk ffmpeg flatpak floorp-bin
 
-    set_color ECEB7B; echo "[Install development tools]"; set_color normal
-    sudo pacman -S --needed python python-pip base-devel npm clang
-    sudo pacman -S --needed dbus-python python-gobject
+    set_color ECEB7B
+    echo "[Install development tools]"
+    set_color normal
+    paru -S --needed python python-pip base-devel npm clang dbus-python python-gobject
 end
 
 function install_misc
-    set_color ECEB7B; echo "[Install fonts]"; set_color normal
-    sudo pacman -S --needed \
+    set_color ECEB7B
+    echo "[Install fonts]"
+    set_color normal
+    paru -S --needed \
         ttf-jetbrains-mono-nerd \
         ttf-liberation \
         noto-fonts-cjk \
@@ -75,33 +89,59 @@ function install_misc
         ttf-cascadia-code-nerd
 end
 
+function install_lsp
+    set_color ECEB7B
+    echo "[Instal LSP and formatter]"
+    set_color normal
+    paru -S --needed tree-sitter ripgrep
+    paru -S --needed \
+        clang \
+        bash-language-server \
+        lua-language-server \
+        pyright \
+        rust-analyzer \
+        stylua \
+        typescript-language-server \
+        vscode-css-languageserver \
+        vscode-json-languageserver \
+        cmake-language-server
+end
+
 function enable_services
-    set_color ECEB7B; echo "[Install network service]"; set_color normal
-    sudo pacman -S --needed dhcpcd
+    set_color ECEB7B
+    echo "[Install network service]"
+    set_color normal
+    paru -S --needed dhcpcd
     sudo systemctl enable --now dhcpcd
 
-    set_color ECEB7B; echo "[Install bluetooth service]"; set_color normal
+    set_color ECEB7B
+    echo "[Install bluetooth service]"
+    set_color normal
     echo "Do you wanna install bluetooth? [y/N]"
     read answer
-    if test "$answer" = "Y" -o "$answer" = "y"
-        sudo pacman -S --needed bluez bluez-utils blueman
+    if test "$answer" = Y -o "$answer" = y
+        paru -S --needed bluez bluez-utils blueman
         systemctl enable --now bluetooth
     end
 
-    set_color ECEB7B; echo "[Install iwd service]"; set_color normal
+    set_color ECEB7B
+    echo "[Install iwd service]"
+    set_color normal
     echo "Do you wanna install iwd? [y/N]"
     read answer
-    if test "$answer" = "Y" -o "$answer" = "y"
+    if test "$answer" = Y -o "$answer" = y
         sudo pacman -S --needed iwd
         sudo systemctl enable --now iwd
     end
 end
 
 function clone_configuations
-    set_color ECEB7B; echo "[Clone dotfiles]"; set_color normal
+    set_color ECEB7B
+    echo "[Clone dotfiles]"
+    set_color normal
     echo "Do you want to clone dotfiles? [Y/n] "
     read answer
-    if test -z "$answer" -o "$answer" = "Y" -o "$answer" = "y"
+    if test -z "$answer" -o "$answer" = Y -o "$answer" = y
         rm -rf $HOME/.dotfiles
         git clone https://github.com/lulkien/dotfiles.git $HOME/.dotfiles
         echo "Please run setup script in $HOME/.dotfiles"
@@ -109,21 +149,23 @@ function clone_configuations
 end
 
 function setup_home_dir
-    set_color ECEB7B; echo "[Setup HOME directory]"; set_color normal
-    if not test -d $HOME/Downloads
-        mkdir $HOME/Downloads
+    set_color ECEB7B
+    echo "[Setup HOME directory]"
+    set_color normal
+
+    set XDG_LIST Downloads Documents Pictures Desktop
+    for xdg_item in $XDG_LIST
+        if not test -d $HOME/$xdg_item
+            mkdir $HOME/$xdg_item
+        end
     end
 
-    if not test -d $HOME/Pictures
-        mkdir $HOME/Pictures
-    end
-
-    echo 'XDG_DESKTOP_DIR="$HOME/Desktop"'      > $HOME/.config/user-dirs.dirs
-    echo 'XDG_DOWNLOAD_DIR="$HOME/Downloads"'   >> $HOME/.config/user-dirs.dirs
-    echo 'XDG_DOCUMENTS_DIR="$HOME/Documents"'  >> $HOME/.config/user-dirs.dirs
-    echo 'XDG_PICTURES_DIR="$HOME/Pictures"'    >> $HOME/.config/user-dirs.dirs
-    echo 'XDG_SCREENSHOTS_DIR="$HOME/Pictures"' >> $HOME/.config/user-dirs.dirs
+    echo 'XDG_DESKTOP_DIR="$HOME/Desktop"' >$HOME/.config/user-dirs.dirs
+    echo 'XDG_DOWNLOAD_DIR="$HOME/Downloads"' >>$HOME/.config/user-dirs.dirs
+    echo 'XDG_DOCUMENTS_DIR="$HOME/Documents"' >>$HOME/.config/user-dirs.dirs
+    echo 'XDG_PICTURES_DIR="$HOME/Pictures"' >>$HOME/.config/user-dirs.dirs
+    echo 'XDG_SCREENSHOTS_DIR="$HOME/Pictures"' >>$HOME/.config/user-dirs.dirs
 end
 
 # MAIN SCRIPT
-prepare_keyring && install_aur_helper && install_desktop_environment && install_misc && enable_services && clone_configuations && setup_home_dir
+prepare_keyring && install_aur_helper && install_desktop_environment && install_misc && install_lsp && enable_services && clone_configuations && setup_home_dir
--- archlinux/install-window-manager/plasma.fish
+++ archlinux/install-window-manager/plasma.fish
@@ -1,20 +1,24 @@
 #!/usr/bin/env fish
 
 function prepare_keyring
-    set_color ECEB7B; echo "[Update package and install latest keyring]"; set_color normal
+    set_color ECEB7B
+    echo "[Update package and install latest keyring]"
+    set_color normal
     sudo pacman -Sy
     sudo pacman -S --needed archlinux-keyring
 end
 
 function install_aur_helper
-    set_color ECEB7B; echo "[Check paru available]"; set_color normal
+    set_color ECEB7B
+    echo "[Check paru available]"
+    set_color normal
     sudo pacman -S --needed base-devel rustup
     rustup default nightly
     if not command -sq paru
         echo 'paru is not installed. Do you wanna install it? [Y/n]'
         read answer
         # If the answer is yes or empty, install paru
-        if test -z "$answer" -o "$answer" = "Y" -o "$answer" = "y"
+        if test -z "$answer" -o "$answer" = Y -o "$answer" = y
             git clone https://aur.archlinux.org/paru.git /tmp/paru
             cd /tmp/paru
             makepkg -si
@@ -23,14 +27,18 @@ function install_aur_helper
 end
 
 function install_desktop_environment
-    set_color ECEB7B; echo "[Install plasma]"; set_color normal
-    sudo pacman -S --needed xorg xorg-xwayland sddm plasma plasma-wayland-session
-    sudo pacman -S --needed konsole dolphin kwallet5 kwalletmanager kate spectacle kdeconnect elisa gwenview ark partitionmanager
+    set_color ECEB7B
+    echo "[Install plasma]"
+    set_color normal
+    paru -S --needed xorg xorg-xwayland sddm plasma plasma-wayland-session
+    paru -S --needed konsole dolphin kwallet5 kwalletmanager kate spectacle kdeconnect elisa gwenview ark partitionmanager
 end
 
 function install_misc
-    set_color ECEB7B; echo "[Install fonts]"; set_color normal
-    sudo pacman -S --needed \
+    set_color ECEB7B
+    echo "[Install fonts]"
+    set_color normal
+    paru -S --needed \
         ttf-jetbrains-mono-nerd \
         ttf-liberation \
         noto-fonts-cjk \
@@ -38,45 +46,70 @@ function install_misc
         otf-codenewroman-nerd \
         ttf-cascadia-code-nerd
 
-    set_color ECEB7B; echo "[Install appliactions]"; set_color normal
-    sudo pacman -S --needed \
+    set_color ECEB7B
+    echo "[Install appliactions]"
+    set_color normal
+    paru -S --needed \
         git fish vim neovim kitty \
-        flatpak \
         htop neofetch lsb-release \
         wget curl openssh rsync wl-clipboard \
         gamemode unzip unarchiver \
         pipewire pipewire-pulse lib32-pipewire wireplumber \
-        discord steam-native-runtime
+        discord steam-native-runtime \
+        flatpak floorp-bin
 
-    paru -S floorp-bin
-
-    set_color ECEB7B; echo "[Install fcitx5]"; set_color normal
+    set_color ECEB7B
+    echo "[Install fcitx5]"
+    set_color normal
     sudo pacman -S --needed fcitx5 kcm-fcitx5 fcitx5-bamboo fcitx5-im
-    echo "QT_IM_MODULE=fcitx"   | sudo tee -a /etc/environment
+    echo "QT_IM_MODULE=fcitx" | sudo tee -a /etc/environment
     echo "XMODIFIERS=@im=fcitx" | sudo tee -a /etc/environment
-    echo "SDL_IM_MODULE=fcitx"  | sudo tee -a /etc/environment
-    echo "GLFW_IM_MODULE=ibus"  | sudo tee -a /etc/environment
+    echo "SDL_IM_MODULE=fcitx" | sudo tee -a /etc/environment
+    echo "GLFW_IM_MODULE=ibus" | sudo tee -a /etc/environment
 
-    set_color ECEB7B; echo "[Install development tools]"; set_color normal
-    sudo pacman -S --needed python python-pip base-devel npm clang
+    set_color ECEB7B
+    echo "[Install development tools]"
+    set_color normal
+    paru -S --needed python python-pip base-devel npm clang dbus-python python-gobject
 end
 
+function install_lsp
+    set_color ECEB7B
+    echo "[Instal LSP and formatter]"
+    set_color normal
+    paru -S --needed tree-sitter ripgrep
+    paru -S --needed \
+        clang \
+        bash-language-server \
+        lua-language-server \
+        pyright \
+        rust-analyzer \
+        stylua \
+        typescript-language-server \
+        vscode-css-languageserver \
+        vscode-json-languageserver \
+        cmake-language-server
+end
 
 function enable_services
-    set_color ECEB7B; echo "[Install bluetooth service]"; set_color normal
+    set_color ECEB7B
+    echo "[Install bluetooth service]"
+    set_color normal
     echo "Do you wanna install bluetooth? [y/N]"
     read answer
-    if test "$answer" = "Y" -o "$answer" = "y"
-        sudo pacman -S --needed bluez bluez-utils
+    if test "$answer" = Y -o "$answer" = y
+        paru -S --needed bluez bluez-utils
         systemctl enable --now bluetooth
     end
 end
 
 function clone_configuations
-    set_color ECEB7B; echo "[Clone dotfiles]"; set_color normal
+    set_color ECEB7B
+    echo "[Clone dotfiles]"
+    set_color normal
     echo "Do you want to clone dotfiles? [Y/n] "
     read answer
-    if test -z "$answer" -o "$answer" = "Y" -o "$answer" = "y"
+    if test -z "$answer" -o "$answer" = Y -o "$answer" = y
         rm -rf $HOME/.dotfiles
         git clone https://github.com/lulkien/dotfiles.git $HOME/.dotfiles
         echo "Please run setup script in $HOME/.dotfiles"
@@ -84,4 +117,4 @@ function clone_configuations
 end
 
 # MAIN SCRIPT
-prepare_keyring && install_aur_helper && install_desktop_environment && install_misc && enable_services && clone_configuations
+prepare_keyring && install_aur_helper && install_desktop_environment && install_misc && install_lsp && enable_services && clone_configuations
