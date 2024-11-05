14ba044fa5b40169bea33b558e8b0ef9c299cd91
lulkien <kien.luuhoang.arch@gmail.com>
2024-11-05 23:08:33 +0700

    Rework script arch-set-fnmode-0

--- others/scripts/arch-set-fnmode-0.sh
+++ others/scripts/arch-set-fnmode-0.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 echo 'options hid_apple fnmode=0' | sudo tee /etc/modprobe.d/hid_apple.conf
-ls -l /etc/mkinitcpio.d
 
-echo "Now, please run mkinitcpio with your linux preset"
-echo "Hint: sudo mkinitcpio -p linuxXYZ"
+presets=($(ls /etc/mkinitcpio.d | grep -E '^linux.*\.preset$' | awk -F'.' '{print $1}'))
+
+select preset in "${presets[@]}"; do
+
+    [[ "$REPLY" =~ ^[0-9]+$ ]] || break
+    [[ "$REPLY" -le 0 || "$REPLY" -gt "${#presets[@]}" ]] && break
+
+    echo -e "\e[1;32msudo mkinitcpio -p linux\e[00m"
+    sudo mkinitcpio -p ${preset}
+
+    break
+
+done
