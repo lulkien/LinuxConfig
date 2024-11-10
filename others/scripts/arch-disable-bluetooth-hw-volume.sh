b97497aba92b6b5baa7e9d9460daa3cb522da0c8
lulkien <kien.luuhoang.arch@gmail.com>
2024-11-11 01:23:58 +0700

    Fix script arch-disable-bluetooth-hw-volume

--- others/scripts/arch-disable-bluetooth-hw-volume.sh
+++ others/scripts/arch-disable-bluetooth-hw-volume.sh
@@ -3,6 +3,6 @@
 WIREPLUMBER_CONFIG_DIR=/etc/wireplumber/wireplumber.conf.d
 BLUEZ_PROPERTIES=${WIREPLUMBER_CONFIG_DIR}/80-bluez-properties.conf
 
-[[ -f $WIREPLUMBER_CONFIG_DIR ]] && sudo mkdir -p $WIREPLUMBER_CONFIG_DIR
+[[ ! -d $WIREPLUMBER_CONFIG_DIR ]] && sudo mkdir -p $WIREPLUMBER_CONFIG_DIR
 
 echo -e "monitor.bluez.properties = {\n    bluez5.enable-hw-volume = false\n}" | sudo tee $BLUEZ_PROPERTIES
