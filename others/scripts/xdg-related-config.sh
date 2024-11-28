dab3e7f235b6d0d5c5c3c2e9432cf1fe8581d2ba
lulkien <kien.luuhoang.arch@gmail.com>
2024-11-28 20:51:39 +0700

    Add adguard block list

--- others/scripts/xdg-related-config.sh
+++ others/scripts/xdg-related-config.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # Set alacritty as default terminal for nemo
-gsettings set org.cinnamon.desktop.default-applications.terminal exec alacritty
+gsettings set org.cinnamon.desktop.default-applications.terminal exec kitty
 
 # Set nemo as default file manager
 xdg-mime default nemo.desktop inode/directory
-
