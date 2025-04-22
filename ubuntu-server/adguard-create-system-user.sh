4d9b0d82e7c4ee9fd477a8d1f69b3a04b25a6984
lulkien <kien.luuhoang.arch@outlook.com>
2025-04-22 17:46:36 +0700

    ubuntu: update script

--- ubuntu-server/adguard-create-system-user.sh
+++ ubuntu-server/adguard-create-system-user.sh
@@ -1,48 +1,148 @@
 #!/usr/bin/env bash
 
+ADGUARD_SERVICE_FILE=/etc/systemd/system/AdGuardHome.service
 ADGUARD_DIR=/opt/AdGuardHome
+ADGUARD_USER=adguard
+BIND_GROUP=lowbind
 
-if [[ ! -d $ADGUARD_DIR ]]; then
-  printf "Why create adguard user when not install AdguardHome?\n"
-  printf "Or I'm wrong? Then feel free to set ADGUARD_DIR yourself.\n"
-  exit 1
-fi
+echo_green() {
+  echo -e '\e[1;32m'"$@"'\e[00m'
+}
 
-if $(id adguard &>/dev/null); then
-  printf "User adguard existed.\n"
-  exit 2
-fi
+echo_red() {
+  echo -e '\e[1;32m'"$@"'\e[00m' >&2
+}
 
-printf "Create user adguard.\n"
+check_run_as_root() {
+  if [[ $EUID -ne 0 ]]; then
+    echo_red "ERROR: This script must be run as root (or with sudo)."
+    return 1
+  fi
+}
 
-sudo useradd -r -s /usr/sbin/nologin -M -d $ADGUARD_DIR adguard
+validate_directory() {
+  if [[ ! -d ${ADGUARD_DIR} ]]; then
+    echo_red "Why create adguard user when not install AdguardHome?"
+    echo_red "Or I'm wrong? Then feel free to set ADGUARD_DIR yourself."
+    return 1
+  fi
+}
 
-printf "\n"
-if $(id adguard &>/dev/null); then
-  printf "It's all done.\n"
-else
-  printf "I can't do it, sorry.\n"
-  exit 3
-fi
+create_user() {
+  echo
 
-printf "\n"
-printf "Now set ownership for ${ADGUARD_DIR}.\n"
-sudo chown -R adguard:adguard $ADGUARD_DIR
+  if $(id ${ADGUARD_USER} &>/dev/null); then
+    echo_green "User ${ADGUARD_USER} existed."
+    return 0
+  fi
 
-printf "\n"
-printf "And set some magic stuff for the AdguardHome binary too.\n"
-sudo setcap 'cap_net_bind_service=+ep' ${ADGUARD_DIR}/AdGuardHome
+  echo_green "Create user ${ADGUARD_USER}."
 
-printf "\n"
-printf "Now, please add these line to your AdGuardHome service file in [Service] section:\n"
-printf "    User=adguard\n"
-printf "    Group=adguard\n"
-printf "\n"
-printf "Your service should be found in /etc/systemd/system.\n"
-printf "Or you can figure it out yourself.\n"
+  useradd -r -s /usr/sbin/nologin -M -d ${ADGUARD_DIR} ${ADGUARD_USER}
 
-printf "\n"
-printf "Very well.\n"
-printf "\n"
-printf "Now, please reload daemon and restart service.\n"
-printf "Good luck, my friend.\n"
+  if $(id ${ADGUARD_USER} &>/dev/null); then
+    echo_green "User ${ADGUARD_USER} is created."
+    return 0
+  fi
+
+  echo_red "User ${ADGUARD_USER} can't be created."
+  return 1
+}
+
+create_group() {
+  echo
+
+  if grep -q "^${BIND_GROUP}:" /etc/group; then
+    echo_green "Group ${BIND_GROUP} existed."
+    return 0
+  fi
+
+  echo_green "Create group ${BIND_GROUP}."
+
+  groupadd ${BIND_GROUP}
+
+  if grep -q "^${BIND_GROUP}:" /etc/group; then
+    echo_green "Group ${BIND_GROUP} is created."
+    return 0
+  fi
+
+  echo_red "Group ${BIND_GROUP} can't be created."
+  return 1
+}
+
+add_user_to_group() {
+  echo
+
+  if groups ${ADGUARD_USER} | grep -q ${BIND_GROUP}; then
+    echo_green "User ${ADGUARD_USER} already in group ${BIND_GROUP}."
+    return 0
+  fi
+
+  echo_green "Add user ${ADGUARD_USER} to group ${BIND_GROUP}."
+
+  usermod -aG ${BIND_GROUP} ${ADGUARD_USER}
+
+  if groups ${ADGUARD_USER} | grep -q ${BIND_GROUP}; then
+    echo_green "User ${ADGUARD_USER} is added to group ${BIND_GROUP}."
+    return 0
+  fi
+
+  echo_red "User ${ADGUARD_USER} can't be added to group ${BIND_GROUP}."
+  return 1
+}
+
+create_bind_rules() {
+  echo
+
+  local bind_file=/etc/authbind/byport/53
+
+  if ! command -v authbind &>/dev/null; then
+    echo_green "Install authbind."
+    apt install authbind
+  fi
+
+  if [[ ! -f ${bind_file} ]]; then
+    echo_green "Create ${bind_file} and set permission."
+    touch ${bind_file}
+    chmod 440 ${bind_file}
+    chown :${BIND_GROUP} ${bind_file}
+    return 0
+  fi
+
+  if [[ "$(ls -l ${bind_file} | awk '{print $4}')" != "${BIND_GROUP}" ]]; then
+    echo_green "Change group for ${bind_file}."
+    chown :${BIND_GROUP} ${bind_file} || return 1
+  fi
+
+  if [[ "$(ls -l ${bind_file} | awk '{print $1}')" != "-r--r-----" ]]; then
+    echo_green "Set permission for ${bind_file}."
+    chmod 440 ${bind_file} || return 1
+  fi
+}
+
+modify_service_file() {
+  echo
+
+  if grep -q 'ExecStart=/opt/AdGuardHome/AdGuardHome' ${ADGUARD_SERVICE_FILE}; then
+    echo_green "Modify service file ${ADGUARD_SERVICE_FILE}"
+
+    sed -i 's|ExecStart=/opt/AdGuardHome/AdGuardHome|ExecStart=authbind --deep /opt/AdGuardHome/AdGuardHome|' ${ADGUARD_SERVICE_FILE}
+
+    if ! grep -q 'ExecStart=authbind --deep /opt/AdGuardHome/AdGuardHome' ${ADGUARD_SERVICE_FILE}; then
+      echo_red "Failed to modify service file ${ADGUARD_SERVICE_FILE}."
+      return 1
+    fi
+
+    systemctl daemon-reload
+  fi
+
+  systemctl restart AdGuardHome
+}
+
+check_run_as_root || exit 1
+validate_directory || exit 1
+create_user || exit 1
+create_group || exit 1
+add_user_to_group || exit 1
+create_bind_rules || exit 1
+modify_service_file || exit 1
