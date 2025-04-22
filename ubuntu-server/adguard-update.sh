1be4bbc1d9b8b371d110de4285d18760a67f13a7
lulkien <kien.luuhoang.arch@outlook.com>
2025-04-22 22:12:09 +0700

    update

--- ubuntu-server/adguard-update.sh
+++ ubuntu-server/adguard-update.sh
@@ -1,42 +1,38 @@
 #!/usr/bin/env bash
 
+BACKUP_DIR=/tmp/AdGuardHome.bak
+
 echo_green() {
   echo -e '\e[1;32m'"$@"'\e[00m'
 }
+
+if [[ $EUID -ne 0 ]]; then
+  echo "ERROR: This script must be run as root (or with sudo)." >&2
+  exit 1
+fi
 
 echo_green "Download latest AdGuardHome binary"
 wget -O '/tmp/AdGuardHome_linux_amd64.tar.gz' 'https://static.adguard.com/adguardhome/release/AdGuardHome_linux_amd64.tar.gz'
 
 echo_green "Stop AdGuardHome service"
-sudo systemctl stop AdGuardHome.service
+systemctl stop AdGuardHome.service
 
 echo_green "Backup AdGuardHome data"
-if [[ ! -d $HOME/server_config/opt/AdGuardHome ]]; then
-  mkdir -p $HOME/server_config/opt/AdGuardHome
+if [[ ! -d ${BACKUP_DIR} ]]; then
+  mkdir -p ${BACKUP_DIR}
 fi
 
-sudo cp /opt/AdGuardHome/AdGuardHome.yaml $HOME/server_config/opt/AdGuardHome
-sudo cp -r /opt/AdGuardHome/data $HOME/server_config/opt/AdGuardHome
+cp /opt/AdGuardHome/AdGuardHome.yaml ${BACKUP_DIR}
+cp -r /opt/AdGuardHome/data ${BACKUP_DIR}
 
 echo_green "Extract new AdGuardHome package"
 tar -C /tmp/ -f /tmp/AdGuardHome_linux_amd64.tar.gz -x -v -z
 
 echo_green "Copy new AdGuardHome binary to /opt/AdGuardHome"
-sudo cp /tmp/AdGuardHome/AdGuardHome /opt/AdGuardHome/
-sudo cp /tmp/AdGuardHome/AdGuardHome.sig /opt/AdGuardHome/
+cp /tmp/AdGuardHome/AdGuardHome /opt/AdGuardHome/
+cp /tmp/AdGuardHome/AdGuardHome.sig /opt/AdGuardHome/
+cp ${BACKUP_DIR}/AdGuardHome.yaml /opt/AdGuardHome/
+cp -r ${BACKUP_DIR}/data /opt/AdGuardHome/
 
-echo_green "Reset ownership for /opt/AdGuardHome"
-sudo chown adguard: -R /opt/AdGuardHome
-
-echo_green "Almost done, but there are a few things you MUST do before you start AdGuardHome.service"
-echo_green "If you run AdGuardHome as another user. You have to reset the ownership"
-echo "  sudo chown <your-adguard-user>: -R /opt/AdGuardHome"
-echo
-
-echo_green "And because that user won't have permission to listen on port 53"
-echo_green "This command allow AdGuardHome to listen on that port"
-echo "  sudo setcap CAP_NET_BIND_SERVICE=+eip /opt/AdGuardHome/AdGuardHome"
-echo
-
-echo_green "Then, you can start AdGuardHome.service"
-echo "  sudo systemctl start AdGuardHome.service"
+echo_green "Start AdGuardHome service"
+systemctl start AdGuardHome.service
